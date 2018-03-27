20c6dcaad0a2744007a4487a219f9dcabcc74169
Fabio <fabio@webeing.net>
2018-03-27 09:29:04 +0200

    phpunit test

---
+++ test/FileExistsTest.php
@@ -0,0 +1,11 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class FileExistsTest extends TestCase
+{
+    public function testFileExists()
+    {
+        $this->assertFileExists('../web/wp/wp-content/themes/moesia/footer.php');
+    }
+}
+?>
